bde5fc595fe615ec705f6dfc3c822630b8930565
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-13 02:22:33 -0200

    c acronym iteration #3 use helper function replaced *pointer with **pointer to be used in realloc()

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -5,29 +5,38 @@
 
 #define BUFFER_LEN 254
 
+void add_letter(char **acronym, const char *letter, int index) {
+  static int len = 0;
+  if (index == 0) {
+    len = 0;
+  }
+
+  if (len % BUFFER_LEN == 0) {
+    int buffer_size = BUFFER_LEN * ((len / BUFFER_LEN) + 1);
+    *acronym = realloc(*acronym, (buffer_size + 2) * sizeof(char));
+    if (acronym == NULL) {
+      free(acronym);
+      printf("Error allocating memory.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  (*acronym)[len] = toupper(*letter);
+  (*acronym)[len + 1] = '\0';
+  len++;
+}
+
 char *abbreviate(const char *phrase) {
   if (phrase == NULL || phrase[0] == '\0') {
     return NULL;
   }
 
   char *acronym = NULL;
-  int len = 0;
   int add = 1;
 
   for (int i = 0; phrase[i] != '\0'; i++) {
     if (add) {
-      if (len % BUFFER_LEN == 0) {
-        int buffer_size = BUFFER_LEN * ((len / BUFFER_LEN) + 1);
-        acronym = realloc(acronym, (buffer_size + 2) * sizeof(char));
-        if (acronym == NULL) {
-          free(acronym);
-          printf("Error allocating memory.\n");
-          exit(EXIT_FAILURE);
-        }
-      }
-      acronym[len] = toupper(phrase[i]);
-      acronym[len + 1] = '\0';  
-      len++;
+      add_letter(&acronym, &phrase[i],  i);
       add = 0;
     }
 
