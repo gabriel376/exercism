660ed2952ac861ca6c91146a4e39c11fd9c69b45
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-13 01:45:08 -0200

    c isogram iteration #2 check NULL replaced inner loop by array of bool

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,32 +1,30 @@
-#include "isogram.h"
 #include <string.h>
 #include <ctype.h>
+#include "isogram.h"
 
-#define END_OF_STRING '\0'
 #define A 65
 #define Z 90
 
-bool is_equal(const char a, const char b) {
-	return toupper(a) == toupper(b);
-}
-
-bool is_alphanumeric(const char a) {
-	char b = toupper(a);
-	return b >= A && b <= Z;
-}
-
 bool is_isogram(const char phrase[]) {
-	if (strlen(phrase) == 0) {
-		return true;
-	}
+  if (phrase == NULL) {
+    return false;
+  }
 
-	for (int i = 0; phrase[i] != END_OF_STRING; i++) {
-		for (int j = i + 1; phrase[j] != END_OF_STRING; j++) {
-			if (is_equal(phrase[i], phrase[j]) && is_alphanumeric(phrase[i])) {
-				return false;
-			}
-		}
-	}
+  if(strlen(phrase) == 0) {
+    return true;
+  }
 
-	return true;
+  bool letters[26] = { false };
+
+  for (int i = 0; phrase[i] != '\0'; i++) {
+    char c = toupper(phrase[i]);
+    if (c >= A && c <= Z) {
+      if (letters[c - A] == true) {
+        return false;
+      }
+      letters[c - A] = true;
+    }
+  }
+  
+  return true;
 }
