d3296eedcd8b4788fc9fb456ee5034a2d5dc8493
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-13 22:47:43 -0200

    rust pythagorean-triplet iteration #2 use struct use methods use iterator use filter

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,21 +1,80 @@
+struct Triangle {
+    pub a: u32,
+    pub b: u32,
+    pub c: u32,
+}
+
+impl Triangle {
+    pub fn new(a: u32, b: u32, c: u32) -> Triangle {
+        Triangle {
+            a,
+            b,
+            c
+        }
+    }
+    
+    pub fn is_pythagorean(&self) -> bool {
+        (self.a * self.a) + (self.b * self.b) == (self.c * self.c)
+    }
+    
+    pub fn sum(&self) -> u32 {
+        self.a + self.b + self.c
+    }
+    
+    pub fn product(&self) -> u32 {
+        self.a * self.b * self.c
+    }
+}
+
+struct Triangles {
+    max: u32,
+    a: u32,
+    b: u32,
+    c: u32,
+}
+
+impl Triangles {
+    pub fn new(max: u32) -> Triangles {
+        if max < 3 {
+            panic!("max should be greater or equal to 3.");
+        }
+        
+        Triangles{
+            max,
+            a: 1,
+            b: 2,
+            c: 2,
+        }
+    }
+}
+
+impl Iterator for Triangles {
+    type Item = Triangle;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.c += 1;
+        if self.c > self.max {
+            self.b += 1;
+            self.c = self.b + 1;
+            if self.c > self.max {
+                self.a += 1;
+                self.b = self.a + 1;
+                self.c = self.b + 1;
+                if self.c > self.max {
+                    return None;
+                }
+            }
+        }
+        
+        Some(Triangle::new(self.a, self.b, self.c))
+    }
+}
+
 pub fn find() -> Option<u32> {
-	let max = 998;
-    for a in 1..max {
-    	for b in a+1..max {
-    		for c in b+1..max {
-    			if is_pythagorean_triplet(a, b, c) && check_cond(a, b, c) {
-    				return Some(a * b * c);
-    			}
-    		}
-    	}
+    let sum = 1000;
+    let triangles = Triangles::new(sum);
+    for t in triangles.filter(|x| x.is_pythagorean() && x.sum() == sum ) {
+        return Some(t.product());
     }
     None
-}
-
-fn is_pythagorean_triplet(a: u32, b: u32, c: u32,) -> bool {
-	(a * a) + (b * b) == (c * c)
-}
-
-fn check_cond(a: u32, b: u32, c: u32,) -> bool {
-	a + b + c == 1000
 }
