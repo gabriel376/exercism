735e438bc467c164f32a013eda4986849d45794b
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-13 22:52:50 -0200

    rust pythagorean-triplet iteration #3 exercism version update use functional programming features

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,80 +1,11 @@
-struct Triangle {
-    pub a: u32,
-    pub b: u32,
-    pub c: u32,
-}
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
-impl Triangle {
-    pub fn new(a: u32, b: u32, c: u32) -> Triangle {
-        Triangle {
-            a,
-            b,
-            c
-        }
-    }
-    
-    pub fn is_pythagorean(&self) -> bool {
-        (self.a * self.a) + (self.b * self.b) == (self.c * self.c)
-    }
-    
-    pub fn sum(&self) -> u32 {
-        self.a + self.b + self.c
-    }
-    
-    pub fn product(&self) -> u32 {
-        self.a * self.b * self.c
-    }
-}
-
-struct Triangles {
-    max: u32,
-    a: u32,
-    b: u32,
-    c: u32,
-}
-
-impl Triangles {
-    pub fn new(max: u32) -> Triangles {
-        if max < 3 {
-            panic!("max should be greater or equal to 3.");
-        }
-        
-        Triangles{
-            max,
-            a: 1,
-            b: 2,
-            c: 2,
-        }
-    }
-}
-
-impl Iterator for Triangles {
-    type Item = Triangle;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        self.c += 1;
-        if self.c > self.max {
-            self.b += 1;
-            self.c = self.b + 1;
-            if self.c > self.max {
-                self.a += 1;
-                self.b = self.a + 1;
-                self.c = self.b + 1;
-                if self.c > self.max {
-                    return None;
-                }
-            }
-        }
-        
-        Some(Triangle::new(self.a, self.b, self.c))
-    }
-}
-
-pub fn find() -> Option<u32> {
-    let sum = 1000;
-    let triangles = Triangles::new(sum);
-    for t in triangles.filter(|x| x.is_pythagorean() && x.sum() == sum ) {
-        return Some(t.product());
-    }
-    None
+pub fn find(sum: u32) -> HashSet<[u32; 3]> {
+    HashSet::from_iter(
+        (1..sum - 3)
+            .flat_map(|a| (a + 1..sum - a).map(move |b| (a, b, sum - a - b)))
+            .filter(|(a, b, c)| a.pow(2) + b.pow(2) == c.pow(2))
+            .map(|(a, b, c)| [a, b, c]),
+    )
 }
