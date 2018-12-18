18becc2778cac4025c44f19587ce65739a1cd1fc
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-18 11:03:04 -0200

    javascript scrabble-score iteration #1

---
+++ javascript/scrabble-score/scrabble-score.js
@@ -0,0 +1,28 @@
+const SCORES_BY_VALUE = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z'],
+};
+
+const pivot = (arr) => {
+    let temp = {};
+    Object.keys(arr).forEach(a => {
+        arr[a].forEach(b => {
+            temp[b] = parseInt(a);
+        });
+    });
+    return temp;
+};
+
+const SCORES_BY_LETTER = pivot(SCORES_BY_VALUE);
+
+module.exports = {
+    score(str) {
+        return Array.from(str.toUpperCase())
+            .reduce((a, b) => a + SCORES_BY_LETTER[b], 0);
+    }
+};
