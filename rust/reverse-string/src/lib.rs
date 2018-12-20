9ba95a888e59c1bfbae1c3ccae0a7255f2af6555
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-20 01:00:55 -0200

    rust reverse-string iteration #2 use functional programming chars(), rev() and collect()

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,10 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let chars: Vec<char> = input.chars().collect();
-    let len = chars.len();
-    let mut reverse = String::from("");
-    for i in 0..len {
-        let c = chars[len-i-1];
-        reverse.push(c);
-    }
-    reverse
+    input.chars().rev().collect()
 }
