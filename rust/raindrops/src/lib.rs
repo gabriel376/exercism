7f613ea45d8485a6da5cf1c8ef5fd6bbe2cf0876
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-20 00:43:10 -0200

    rust raindrops iteration #2 use closure use match

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,28 +1,16 @@
-pub fn raindrops(n: usize) -> String {
-	let three_str = String::from("Pling");
-	let five_str = String::from("Plang");
-	let seven_str = String::from("Plong");
-	let mut found = false;
-	let mut final_str = String::from("");
+pub fn raindrops(num: usize) -> String {
+    let mut msg = String::from("");
 
-	if n % 3 == 0 {
-		final_str.push_str(&three_str);
-		found = true;
-	}
+    let mut drop = |fac, str| {
+        if num % fac == 0 {
+            msg.push_str(str);
+            return true;
+        }
+        return false;
+    };
 
-	if n % 5 == 0 {
-		final_str.push_str(&five_str);
-		found = true;
-	}
-
-	if n % 7 == 0 {
-		final_str.push_str(&seven_str);
-		found = true;
-	}
-
-	if !found {
-		final_str = n.to_string();
-	}
-
-	final_str
+    match (drop(3, "Pling"), drop(5, "Plang"), drop(7, "Plong")) {
+        (false, false, false) => num.to_string(),
+        (_, _, _) => msg,
+    }
 }
