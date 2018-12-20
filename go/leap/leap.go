23ca2979fd764ec605716964a82826996b8d4812
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-20 10:34:01 -0200

    go leap iteration #2 improve readability

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,5 +2,14 @@ package leap
 
 // IsLeapYear calculates if given year is a leap year
 func IsLeapYear(year int) bool {
-	return (year%4 == 0 && year%100 != 0) || year%400 == 0
+	if year%4 != 0 {
+		return false
+	}
+	if year%100 != 0 {
+		return true
+	}
+	if year%400 == 0 {
+		return true
+	}
+	return false
 }
