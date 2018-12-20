f812b85213df8f59995ceeff4b6b742d0b2207e6
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-20 12:23:23 -0200

    rust nth-prime iteration #2 use functional programming use range, filter, all, nth

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,26 +1,6 @@
-pub fn nth(n: u32) -> Option<u32> {
-	let mut count = 0;
-
-	if n == 0 {
-		return None;
-	}
-
-	for i in 2..<u32>::max_value() {
-		if is_prime(i) {
-			count = count + 1;
-			if count == n {
-				return Some(i);
-			}
-		}
-	}
-	None
-}
-
-fn is_prime(n: u32) -> bool {
-	for i in 2..n-1 {
-		if n % i == 0 {
-			return false;
-		}
-	}
-	true
+pub fn nth(n: u32) -> u32 {
+    (2..<u32>::max_value())
+        .filter(|a| (2..a - 1).all(|b| a % b != 0))
+        .nth(n as usize)
+        .unwrap()
 }
