5b707858a2e8cb48fa506a8ff8bde89aa3ec5177
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2018-12-21 13:36:49 -0200

    rust bob iteration #2 refactoring use functional programming version update

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,47 +1,16 @@
 pub fn reply(message: &str) -> &str {
-	if has_alphanumeric(&message) && !is_question(&message) {
-		return "Fine. Be that way!";
+    let has_alphabetic = message.chars().any(|c| c.is_alphabetic());
+    let has_alphanumeric = message.chars().any(|c| c.is_alphanumeric());
 
-	} else if is_question(&message) && !is_yelling(&message) {
-		return "Sure.";
+    let is_talking = has_alphanumeric;
+    let is_yelling = has_alphabetic && message.to_uppercase() == message;
+    let is_question = message.trim().ends_with("?");
 
-	} else if is_question(&message) && is_yelling(&message) {
-		return "Calm down, I know what I'm doing!";
-
-	} else if !is_question(&message) && is_yelling(&message) {
-		return "Whoa, chill out!";
-
-	} else {
-		return "Whatever.";
-	}
-}
-
-fn is_question(message: &str) -> bool {
-	let chars: Vec<char> = message.trim().chars().collect();
-	let len = chars.len();
-	message.contains("?") && chars[len-1] == '?'
-}
-
-fn is_yelling(message: &str) -> bool {
-	message.to_uppercase() == message && has_letter(message)
-}
-
-fn has_alphanumeric (message: &str) -> bool {
-	let chars: Vec<char> = message.chars().collect();
-	for c in chars {
-		if c.is_alphanumeric() {
-			return false;
-		}
-	}
-	true
-}
-
-fn has_letter(message: &str) -> bool {
-	let chars: Vec<char> = message.chars().collect();
-	for c in chars {
-		if c.is_alphabetic() {
-			return true;
-		}
-	}
-	false
+    match (is_talking, is_yelling, is_question) {
+        (_, true, true) => "Calm down, I know what I'm doing!",
+        (_, true, _) => "Whoa, chill out!",
+        (_, _, true) => "Sure.",
+        (false, _, _) => "Fine. Be that way!",
+        (_, _, _) => "Whatever.",
+    }
 }
