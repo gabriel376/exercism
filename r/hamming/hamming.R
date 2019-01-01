d7b7a6aaa93e98207285c0a4ecb78d70d6209df6
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-01 17:09:56 -0200

    r hamming iteration #2 remove variables and use a single operation

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,16 +1,4 @@
 hamming <- function(strand1, strand2) {
-    strand1.len = nchar(strand1)
-    strand2.len = nchar(strand2)
-
-    stopifnot(strand1.len == strand2.len)
-    
-    strand1.list = strsplit(strand1, "")
-    strand2.list = strsplit(strand2, "")
-
-    strand1.vector = unlist(strand1.list)
-    strand2.vector = unlist(strand2.list)
-    
-    diff = strand1.vector != strand2.vector
-
-    sum(diff)
+    stopifnot(nchar(strand1) == nchar(strand2))
+    sum(strsplit(strand1, "")[[1]] != strsplit(strand2, "")[[1]])
 }
