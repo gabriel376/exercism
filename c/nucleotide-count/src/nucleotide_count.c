36bb2838059a8ba48cb0d6d9240a46f57b2c0435
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-01 23:43:50 -0200

    c nucleotide-count iteration #2 simplify solution

--- c/nucleotide-count/src/nucleotide_count.c
+++ c/nucleotide-count/src/nucleotide_count.c
@@ -1,78 +1,48 @@
-#include "nucleotide_count.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include "nucleotide_count.h"
 
-Nucleotide* Nucleotide_new(const char symbol) {
-  Nucleotide *nucleotide = malloc(1 * sizeof(Nucleotide));
-  nucleotide->symbol = symbol;
-  nucleotide->count = 0;
-  return nucleotide;
-}
+typedef struct {
+  char symbol;
+  int count;
+} Base;
 
-Nucleotide** Nucleotides_new(const char *symbols) {
-  int len = strlen(symbols);
-  Nucleotide **nucleotides = malloc(len * sizeof(Nucleotide*));
+int find(Base *bases, int len, char symbol) {
   for (int i = 0; i < len; i++) {
-    Nucleotide *nucleotide = Nucleotide_new(symbols[i]);
-    nucleotides[i] = nucleotide;
-  }
-  return nucleotides;
-}
-
-int Nucleotides_find(const char symbol, const int len, Nucleotide **nucleotides) {
-  for (int i = 0; i < len; i++) {
-    if (nucleotides[i]->symbol == symbol) {
+    if (bases[i].symbol == symbol) {
       return i;
     }
   }
   return -1;
 }
 
-bool Nucleotides_count(const char *strand, const int len, Nucleotide **nucleotides) {
-  for (int i = 0; strand[i] != '\0'; i++) {
-    int index = Nucleotides_find(strand[i], len, nucleotides);
-    if (index < 0) {
-      return false;
-    }
-    nucleotides[index]->count++;
-  }
-  return true;
-}
-
-char* Nucleotides_summarize(const int len, Nucleotide **nucleotides) {
-  char *summary = calloc(5 * len, sizeof(char));
-  for (int i = 0; i < len; i++) {
-    char buffer[4];
-    sprintf(buffer, "%c:%d", nucleotides[i]->symbol, nucleotides[i]->count);
-    if (i == 0) {
-      sprintf(summary, "%s", buffer);
-    } else {
-      sprintf(summary, "%s %s", summary, buffer);
-    }
-  }
-  return summary;
-}
-
-void Nucleotides_destroy(const int len, Nucleotide **nucleotides) {
-  for (int i = 0; i < len; i++) {
-    free(nucleotides[i]);
-  }
-  free(nucleotides);
-}
-
 char* count(const char *strand) {
-  const char *symbols = "ACGT";
-  const int len = 4;
-  Nucleotide **nucleotides = Nucleotides_new(symbols);
-  bool status = Nucleotides_count(strand, len, nucleotides);
-  if (status == false) {
-    Nucleotides_destroy(len, nucleotides);
-    char *empty = calloc(1, sizeof(char));
-    return empty;
+  int len = 4;
+  Base bases[] = {
+    { .symbol = 'A', .count = 0, },
+    { .symbol = 'C', .count = 0, },
+    { .symbol = 'G', .count = 0, },
+    { .symbol = 'T', .count = 0, },
+  };
+
+  char *summary = calloc(6 * len, sizeof(char));
+
+  for (int i = 0; strand[i] != '\0'; i++) {
+    int index = find(bases, len, strand[i]);
+    if (index >= 0) {
+      bases[index].count++;
+    } else {
+      return summary;
+    }
   }
-  char *summary = Nucleotides_summarize(len, nucleotides);
-  Nucleotides_destroy(len, nucleotides);
+
+  int offset = 0;
+  for (int i = 0; i < len; i++) {
+    offset += sprintf(summary + offset, "%c:%d", bases[i].symbol, bases[i].count);
+    if (i + 1 < len) {
+      offset += sprintf(summary + offset, " ");
+    }
+  }
+
   return summary;
 }
