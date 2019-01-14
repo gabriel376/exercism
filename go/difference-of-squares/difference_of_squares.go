ed3f27cfd666d9925de9072a585a54a210cf0330
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-14 15:46:54 -0200

    go difference-of-squares iteration #2 fix golint issues

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,5 +1,7 @@
 package diffsquares
 
+// SquareOfSum calculates the square
+// of the sum of the first n integers.
 func SquareOfSum(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -8,6 +10,8 @@ func SquareOfSum(n int) int {
 	return sum * sum
 }
 
+// SumOfSquares calculates the sum
+// of the square of the first n integers.
 func SumOfSquares(n int) int {
 	sum := 0
 	for i := 1; i <= n; i++ {
@@ -16,8 +20,8 @@ func SumOfSquares(n int) int {
 	return sum
 }
 
+// Difference calculates the difference between
+// the SquareOfSum and SumOfSquares.
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
-
-
