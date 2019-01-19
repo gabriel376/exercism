7de674814b9eea744363b83b2e5a3c633e82bdff
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-19 03:10:01 -0200

    common-lisp leap iteration #1

---
+++ common-lisp/leap/leap-year.lisp
@@ -0,0 +1,11 @@
+(defpackage #:leap
+  (:use #:common-lisp)
+  (:export #:leap-year-p))
+(in-package #:leap)
+
+(defun leap-year-p (year)
+  (cond
+    ((> (rem year 4) 0) nil)
+    ((> (rem year 100) 0) t)
+    ((= (rem year 400) 0) t)
+    (t nil)))
