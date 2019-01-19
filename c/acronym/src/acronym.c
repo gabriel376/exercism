5d75f67778f65415bbf227957cd4a3034857c8ba
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-19 02:11:00 -0200

    c acronym iteration #4 simplify solution

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -1,47 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 
-#define BUFFER_LEN 254
-
-void add_letter(char **acronym, const char *letter, int index) {
-  static int len = 0;
-  if (index == 0) {
-    len = 0;
-  }
-
-  if (len % BUFFER_LEN == 0) {  
-    int buffer_size = BUFFER_LEN * ((len / BUFFER_LEN) + 1);
-    *acronym = realloc(*acronym, (buffer_size + 2) * sizeof(char));
-    if (acronym == NULL) {
-      free(acronym);
-      printf("Error allocating memory.\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  (*acronym)[len] = toupper(*letter);
-  (*acronym)[len + 1] = '\0';  
-  len++;
-}
-
-char *abbreviate(const char *phrase) {
-  if (phrase == NULL || phrase[0] == '\0') {
+char *abbreviate(const char * const input) {
+  if (input == NULL || input[0] == '\0') {
     return NULL;
   }
 
   char *acronym = NULL;
-  int add = 1;
+  int len = 0;
 
-  for (int i = 0; phrase[i] != '\0'; i++) {
-    if (add) {
-      add_letter(&acronym, &phrase[i],  i);
-      add = 0;
-    }
-
-    if (phrase[i] == ' ' || phrase[i] == '-') {
-      add = 1;
+  for (int i = 0; input[i] != '\0'; i++) {
+    if (i == 0 || input[i-1] == ' ' || input[i-1] == '-') {
+      len++;
+      acronym = realloc(acronym, len + 1);
+      acronym[len-1] = toupper(input[i]);
+      acronym[len] = '\0';
     }
   }
 
