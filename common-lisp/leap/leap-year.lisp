15071b8d12b8cfc4b1d6cf49e22041d1ea73a12f
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-01-19 18:43:29 -0200

    common-lisp leap iteration #2 replace > operator with /= remove default cond clause

--- common-lisp/leap/leap-year.lisp
+++ common-lisp/leap/leap-year.lisp
@@ -5,7 +5,6 @@
 
 (defun leap-year-p (year)
   (cond
-    ((> (rem year 4) 0) nil)
-    ((> (rem year 100) 0) t)
-    ((= (rem year 400) 0) t)
-    (t nil)))
+    ((/= (rem year 4) 0) nil)
+    ((/= (rem year 100) 0) t)
+    ((= (rem year 400) 0) t)))
