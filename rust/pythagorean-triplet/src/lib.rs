9822b6e6139709ddbf1c1c25968806020d7a261c
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-10 20:18:59 -0200

    rust pythagorean-triplet iteration #5 use crate rayon for parallelism

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,14 +1,17 @@
 use std::collections::HashSet;
-use std::iter::FromIterator;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    HashSet::from_iter((1..sum / 3).filter_map(|a| {
-        let b = ((sum - a).pow(2) - a.pow(2)) / (2 * (sum - a));
-        let c = sum - a - b;
+    (1..sum / 3)
+        .into_par_iter()
+        .filter_map(|a| {
+            let b = ((sum - a).pow(2) - a.pow(2)) / (2 * (sum - a));
+            let c = sum - a - b;
 
-        match a < b && a.pow(2) + b.pow(2) == c.pow(2) {
-            true => Some([a, b, c]),
-            false => None,
-        }
-    }))
+            match a < b && a.pow(2) + b.pow(2) == c.pow(2) {
+                true => Some([a, b, c]),
+                false => None,
+            }
+        })
+        .collect::<HashSet<[u32; 3]>>()
 }
