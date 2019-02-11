5b82bb1c51fead3434ff19c1da6661856234c446
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-11 00:52:02 -0200

    python largest-series-product iteration #1

---
+++ python/largest-series-product/largest_series_product.py
@@ -0,0 +1,10 @@
+from operator import mul
+from functools import reduce
+
+def largest_product(series, size):
+    if size < 0:
+        raise ValueError('Invalid size')
+
+    chunks = (series[i:i + size] for i in range(len(series) - size + 1))
+    products = (reduce(mul, map(int, chunk), 1) for chunk in map(list, chunks))
+    return max(products)
