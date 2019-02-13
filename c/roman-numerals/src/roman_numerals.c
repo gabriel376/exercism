2b7981ce6afe75fca539468e3d2835fda4281edf
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-13 20:27:30 -0200

    c roman-numerals iteration #1

---
+++ c/roman-numerals/src/roman_numerals.c
@@ -0,0 +1,43 @@
+#include "roman_numerals.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int value;
+    const char *symbol;
+} Numeral;
+
+const Numeral NUMERALS[] = {
+  { 1000, "M"  },
+  {  900, "CM" },
+  {  500, "D"  },
+  {  400, "CD" },
+  {  100, "C"  },
+  {   90, "XC" },
+  {   50, "L"  },
+  {   40, "XL" },
+  {   10, "X"  },
+  {    9, "IX" },
+  {    5, "V"  },
+  {    4, "IV" },
+  {    1, "I"  },
+};
+
+char* to_roman_numeral(int number) {
+  char *symbols = calloc(32, sizeof(char));
+
+  int index = 0;
+  while (number > 0) {
+    Numeral numeral = NUMERALS[index];
+
+    if (number < numeral.value) {
+      index++;
+      continue;
+    }
+
+    number -= numeral.value;
+    strcat(symbols, numeral.symbol);
+  }
+
+  return symbols;
+}
