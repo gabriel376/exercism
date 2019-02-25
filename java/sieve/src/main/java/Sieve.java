d8090ba5dd056f4a253adfe75abef364c3843c8c
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-02-25 04:26:11 -0300

    java sieve iteration #1

---
+++ java/sieve/src/main/java/Sieve.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.BitSet;
+
+class Sieve {
+    private int limit;
+
+    Sieve(int limit) {
+        this.limit = limit + 1;
+    }
+
+    List<Integer> getPrimes() {
+        BitSet bits = new BitSet();
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 2; i < limit; i++) {
+            if (!bits.get(i)) {
+                list.add(i);
+            }
+            for (int j = i+i; j < limit; j+=i) {
+                bits.set(j);
+            }
+        }
+
+        return list;
+    }
+}
