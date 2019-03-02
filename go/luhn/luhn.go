1d9c5169f91e1c3de7c629fae9b4c14186f94b17
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-03-02 15:27:16 -0300

    go luhn iteration #2 use guard clauses

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -4,28 +4,31 @@ import (
 	"unicode"
 )
 
-// Valid calculates if a given number is valid
-// per the Luhn formula.
+// Valid calculates if a given number
+// is valid per the Luhn formula.
 func Valid(input string) bool {
 	sum := 0
-	digits := 0
+	count := 0
 
 	for _, char := range input {
-		if unicode.IsDigit(char) {
-			digit := int(char - '0')
-			if digits%2 == len(input)%2 {
-				digit *= 2
-				if digit > 9 {
-					digit -= 9
-				}
-			}
-			sum += digit
-			digits++
+		if unicode.IsSpace(char) {
+			continue
+		}
 
-		} else if !unicode.IsSpace(char) {
+		if !unicode.IsDigit(char) {
 			return false
 		}
+
+		digit := int(char - '0')
+		if count%2 == len(input)%2 {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		sum += digit
+		count++
 	}
 
-	return digits > 1 && sum%10 == 0
+	return count > 1 && sum%10 == 0
 }
