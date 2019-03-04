692d53d0fb6ea61607e54543ba1e9f18b55a6ac3
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-03-04 04:42:46 -0300

    rust nth-prime iteration #3 improve performance by using sqrt() use trait and impl for u32

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,6 +1,26 @@
+pub trait Sqrt<T> {
+    fn sqrt(self: &Self) -> Self;
+}
+
+pub trait Prime<T> {
+    fn is_prime(self: &Self) -> bool;
+}
+
+impl Sqrt<u32> for u32 {
+    fn sqrt(&self) -> u32 {
+        (*self as f32).sqrt() as u32 
+    }
+}
+
+impl Prime<u32> for u32 {
+    fn is_prime(&self) -> bool {
+        (2..=self.sqrt()).all(|n| self % n != 0)
+    }
+}
+
 pub fn nth(n: u32) -> u32 {
-    (2..<u32>::max_value())
-        .filter(|a| (2..a - 1).all(|b| a % b != 0))
+    (2..)
+        .filter(u32::is_prime)
         .nth(n as usize)
-        .unwrap()
+        .unwrap_or(0)
 }
