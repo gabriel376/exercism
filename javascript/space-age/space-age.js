24c7f4f273638d1862f77b8e092878af7cbfca6a
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-03-19 09:03:21 -0300

    javascript space-age iteration #1

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,15 @@
+const PLANETS = {
+    earth:    1,
+    mercury:  0.2408467,
+    venus:    0.61519726,
+    mars:     1.8808158,
+    jupiter:  11.862615,
+    saturn:   29.447498,
+    uranus:   84.016846,
+    neptune:  164.79132,
+};
+
+export const age = (planet, seconds) => {
+    let result = seconds / PLANETS[planet] / 31557600;
+    return Number(result.toFixed(2));
+};
