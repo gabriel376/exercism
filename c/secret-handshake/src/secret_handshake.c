53346fb8cc6da17114be77767778d5d3a6d2f588
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-03-27 17:08:28 -0300

    c secret-handshake iteration #3 small refactoring and bug fix

--- c/secret-handshake/src/secret_handshake.c
+++ c/secret-handshake/src/secret_handshake.c
@@ -1,26 +1,26 @@
 #include <stdlib.h>
 #include "secret_handshake.h"
 
+#define LEN(arr) sizeof(arr) / sizeof(arr[0])
+
 static const char *COMMANDS[] = {
   "wink",
   "double blink",
   "close your eyes",
   "jump",
-  NULL
 };
 
-static const int LEN = (sizeof(COMMANDS) / sizeof(char*)) - 1;
-
 const char** commands(const int secret) {
-  const char **result = malloc(LEN * sizeof(char*));
-  const int rev = secret & (1 << LEN);
+  const int len = LEN(COMMANDS);
+  const char **result = calloc(len + 1, sizeof(char*));
+  const int rev = secret & (1 << len);
   int count = 0;
 
-  for (int i = rev ? LEN - 1 : 0; rev ? i >= 0 : i < LEN; rev ? i-- : i++) {
-      if (secret & (1 << i)) {
-        result[count++] = COMMANDS[i];
-      }
+  for (int i = rev ? len - 1 : 0; rev ? i >= 0 : i < len; rev ? i-- : i++) {
+    if (secret & (1 << i)) {
+      result[count++] = COMMANDS[i];
     }
+  }
 
   return result;
 }
