6d3ec36b71127f4b1683b748251a7468411a149f
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-04-03 16:44:34 -0300

    bash hamming iteration #1

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo 'Usage: hamming.sh <string1> <string2>'
+    exit 1
+fi
+
+if [[ ${#1} -ne ${#2} ]]; then
+    echo 'left and right strands must be of equal length'
+    exit 1
+fi
+
+count=0
+
+for (( i=0; i<${#1}; i++ )); do
+    if [[ ${1:i:1} != ${2:i:1} ]]; then
+        ((count++))
+    fi
+done
+
+echo $count
