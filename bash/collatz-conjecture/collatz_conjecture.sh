ec259dc53a0fe8ed2545915e495cfb0317c0ed08
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-05-08 22:20:27 -0300

    bash reverse-string iteration #2 small refactoring

--- bash/collatz-conjecture/collatz_conjecture.sh
+++ bash/collatz-conjecture/collatz_conjecture.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
-
 function main() {
     local num=$1
     local count=0
 
-    if [ $num -le 0 ]; then
+    if (( num <= 0 )); then
         echo "Error: Only positive numbers are allowed"
         return 1
     fi
 
-    while [ $num -gt 1 ]; do
-        count=$(( $count + 1 ))
-        if (( $num % 2 == 0)); then
-            num=$(( $num / 2 ))
+    while (( num > 1 )); do
+        (( count += 1 ))
+        if (( num % 2 == 0 )); then
+            (( num /= 2 ))
         else
-            num=$(( 3 * $num + 1 ))
+            (( num *= 3 ))
+            (( num += 1 ))
         fi
     done
 
-    echo $count
+    echo "$count"
 }
 
-main $1
+main "$1"
