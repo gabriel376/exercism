fa18e7ab515112171c7734cf0ad78c02e3afab94
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-05-25 12:59:31 -0300

    python ocr-numbers iteration #2 improve readability and performance

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -6,25 +6,23 @@ DIGITS = [' _     _  _     _  _  _  _  _ ',
           '                              ',
 ]
 
-def convert(data):
-    if len(data) % HEIGHT or any(len(row) % WIDTH for row in data):
-        raise ValueError('Invalid data')
-
-    result = ''
-    for i, digit in enumerate(get_digits(data)):
-        result += convert_digit(digit)
-        if (i + 1) * WIDTH % len(data[0]) == 0:
-            result += ','
-
-    return result[:-1]
-
-def get_digits(data):
+def split_digits(data):
     for x in range(0, len(data), HEIGHT):
         for y in range(0, len(data[0]), WIDTH):
             yield [dig[y:y+WIDTH] for dig in data[x:x+HEIGHT]]
 
-def convert_digit(digit):
-    for i, val in enumerate(get_digits(DIGITS)):
-        if val == digit:
-            return str(i)
-    return '?'
+def convert_digits(digits, width):
+    base = list(split_digits(DIGITS))
+    check = enumerate(split_digits(digits))
+
+    for i, digit in check:
+        yield str(base.index(digit)) if digit in base else '?'
+        yield ',' if (i + 1) * WIDTH % width == 0 else ''
+
+def convert(data):
+    if len(data) % HEIGHT or any(len(row) % WIDTH for row in data):
+        raise ValueError('Invalid data')
+
+    width = len(data[0])
+    digits = convert_digits(data, width)
+    return ''.join(digits)[:-1]
