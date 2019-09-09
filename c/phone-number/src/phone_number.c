675250d93ac1421212087cbd96da0ce1f01bae50
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-09-09 19:28:32 -0300

    c phone-number iteration #2 small refactoring use builtin strchr()

--- c/phone-number/src/phone_number.c
+++ c/phone-number/src/phone_number.c
@@ -5,20 +5,13 @@
 #include <string.h>
 #include <ctype.h>
 
-#define ERROR "0000000000"
-#define PHONE_LEN 10
-#define MAX_LEN PHONE_LEN + 1
+#define ERROR       "0000000000"
+#define PHONE_LEN   10
+#define MAX_LEN     PHONE_LEN + 1
 
 static char* error(char *data) {
     sprintf(data, "%s", ERROR);
     return data;
-}
-
-static int check_char(char actual, const char *expected) {
-    while (*expected)
-        if (actual == *expected++)
-            return 1;
-    return 0;
 }
 
 char* phone_number_clean(const char *phone) {
@@ -27,23 +20,27 @@ char* phone_number_clean(const char *phone) {
     int len = 0;
 
     for (int i = 0; (chr = phone[i]) != '\0'; i++) {
-        if (check_char(chr, "+.-( )")) {
+        // ignore these characters
+        if (strchr("+.-( )", chr)) {
            continue;
         }
 
-       if (!isdigit(chr) || len >= MAX_LEN) {
+        // error if not a digit or exceed maximum length
+        if (!isdigit(chr) || len >= MAX_LEN) {
             return error(result);
         }
 
+        // append char to final string
         len += sprintf(result + len, "%c", chr);
     }
 
     if (len == MAX_LEN) {
         // check country code
-        if (!check_char(result[0], "1")) {
+        if (!strchr("1", result[0])) {
             return error(result);
         }
 
+        // remove country code
         len = sprintf(result, "%s", result + 1);
     }
 
@@ -52,8 +49,8 @@ char* phone_number_clean(const char *phone) {
     }
 
     // check area code and exchange code
-    if (!check_char(result[0], "23456789")
-     || !check_char(result[3], "23456789")) {
+    if (!strchr("23456789", result[0])
+     || !strchr("23456789", result[3])) {
         return error(result);
     }
 
