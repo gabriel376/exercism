5425a9cbce878a5742c8599ebde0a3c3ef3a60a3
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-09-12 19:28:16 -0300

    go clock iteration #3 refactoring version update

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,41 +1,39 @@
+// Package clock handles basic time operations.
 package clock
 
 import (
-    "fmt"
+	"fmt"
 )
 
-// Clock represents a clock object
-type Clock struct {
-    minutes  int
-}
+// Clock represents a clock object.
+type Clock int
 
 // New creates a new Clock.
 func New(hh, mm int) Clock {
-    day := 24 * 60
-    minutes := (hh*60 + mm) % day
-    if (minutes < 0) {
-        minutes += day
-    }
+	day := 24 * 60
 
-    return Clock {
-        minutes: minutes,
-    }
+	minutes := hh*60 + mm
+	minutes %= day
+	minutes += day
+	minutes %= day
+
+	return Clock(minutes)
 }
 
 // String displays the Clock.
 func (clock Clock) String() string {
-    return fmt.Sprintf(
-        "%02d:%02d",
-        clock.minutes / 60,
-        clock.minutes % 60);
+	return fmt.Sprintf(
+		"%02d:%02d",
+		clock/60,
+		clock%60)
 }
 
 // Add advances the Clock by given minutes.
 func (clock Clock) Add(mm int) Clock {
-    return New(0, clock.minutes + mm);
+	return New(0, int(clock)+mm)
 }
 
 // Subtract retreats the Clock by given minutes.
 func (clock Clock) Subtract(mm int) Clock {
-    return New(0, clock.minutes - mm);
+	return New(0, int(clock)-mm)
 }
