90f4f3917e9301002b87fb7414c3d80d24212e0b
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-09-26 20:45:15 -0300

    go nth-prime iteration #2 small refactoring add cache to improve performance

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -2,23 +2,26 @@ package prime
 
 import "math"
 
-// Nth returns the nth prime.
-func Nth(number int) (int, bool) {
-	count := 0
+var primes = []int{2}
 
-	for n := 2; number > 0; n++ {
-		if isPrime(n) {
-			count++
+// Nth returns the nth prime.
+func Nth(nth int) (int, bool) {
+	count := len(primes)
+
+	for num := primes[count-1] + 1; nth > 0; num++ {
+		if count >= nth {
+			return primes[nth-1], true
 		}
-		if number == count {
-			return n, true
+		if isPrime(num) {
+			primes = append(primes, num)
+			count++
 		}
 	}
 
 	return 0, false
 }
 
-// isPrime checks if a given number is prime.
+// isPrime checks is a given number is prime.
 func isPrime(number int) bool {
 	limit := int(math.Sqrt(float64(number)))
 
