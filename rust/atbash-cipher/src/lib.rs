1c0e00e2405850b36c934013f515b58d61e9d776
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-09-27 20:45:20 -0300

    rust atbash-cipher iteration #2 small refactoring

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,11 +1,9 @@
 pub fn encode(text: &str) -> String {
-    text.to_lowercase().chars()
+    text.to_lowercase()
+        .chars()
         .filter_map(flip)
-        .collect::<Vec<char>>()
-        .chunks(5)
-        .collect::<Vec<_>>()
-        .join(&' ')
-        .into_iter()
+        .enumerate()
+        .flat_map(group)
         .collect()
 }
 
@@ -15,10 +13,21 @@ pub fn decode(text: &str) -> String {
         .collect()
 }
 
-fn flip(c: char) -> Option<char> {
-    match c {
-        '0'...'9' => Some(c),
-        'a'...'z' => Some((b'a' + b'z' - c as u8) as char),
+fn flip(chr: char) -> Option<char> {
+    match chr {
+        '0'...'9' => Some(chr),
+        'a'...'z' => Some((b'a' + b'z' - chr as u8) as char),
         _ => None,
     }
+}
+
+fn group((index, chr): (usize, char)) -> std::vec::IntoIter<char> {
+    let width = 5;
+    let mut vec = vec![chr];
+
+    if index > 0 && index % width == 0 {
+        vec.insert(0, ' ');
+    }
+
+    vec.into_iter()
 }
