10d222098c437ab73f0a66800e7601271d5fb7af
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-09-30 21:12:11 -0300

    python protein-translation iteration #2 small refactoring use generators improve readability

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -8,14 +8,18 @@ PROTEINS_BY_NAME = {
     'Tryptophan': ['UGG'],
 }
 
-PROTEINS_BY_CODON = {v: k for k in PROTEINS_BY_NAME for v in PROTEINS_BY_NAME[k]}
+PROTEINS_BY_CODON = {codon: protein
+                     for (protein, codons) in PROTEINS_BY_NAME.items()
+                     for codon in codons}
+
+def codons(strand):
+    step = 3
+    limit = len(strand)
+    for i in range(0, limit, step):
+        codon = strand[i:i+step]
+        if codon not in PROTEINS_BY_CODON:
+            break
+        yield codon
 
 def proteins(strand):
-    step = 3
-    until = len(strand)
-    codons = [strand[i:i+step] for i in range(0, until, step) if i+step <= until]
-    stop = [i for i, e in enumerate(codons) if not e in PROTEINS_BY_CODON]
-    if len(stop) > 0:
-        codons = codons[0:stop[0]]
-    proteins = [PROTEINS_BY_CODON[c] for c in codons]
-    return proteins
+    return [PROTEINS_BY_CODON[c] for c in codons(strand)]
