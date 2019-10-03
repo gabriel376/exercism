b8253adb1062fa88aca160469465196918ab184a
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-10-02 22:10:48 -0300

    python palindrome-products iteration #2 small refactoring version update

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,22 +1,21 @@
-def smallest_palindrome(min_factor, max_factor):
-    return get_palindrome(min_factor, max_factor, False)
+def smallest(min_factor, max_factor):
+    return calculate(min_factor, max_factor, False)
 
-def largest_palindrome(min_factor, max_factor):
-    return get_palindrome(min_factor, max_factor, True)
+def largest(min_factor, max_factor):
+    return calculate(min_factor, max_factor, True)
 
-def get_palindrome(low, high, rev):
-    palindrome = 0
-    factors = []
+def calculate(low, high, rev):
+    if low > high:
+        raise ValueError("min_factor must be >= max_factor")
+
     multiples = range(low**2, high**2+1)
-
     if rev:
         multiples = reversed(multiples)
 
     for x in multiples:
         if str(x) == str(x)[::-1]:
-            palindrome = x
             factors = [(y, x/y) for y in range(low, high+1) if x%y==0 and low <= x/y <= high]
             if factors:
-                return palindrome, factors
+                return x, factors
 
-    raise ValueError("could not find any valid palindrome")
+    return None, []
