0bb5d8169efa3edd3a87000eb1f7e002e887d519
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-10-03 22:57:02 -0300

    java sieve iteration #2 small refactoring

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -6,18 +6,18 @@ class Sieve {
     private int limit;
 
     Sieve(int limit) {
-        this.limit = limit + 1;
+        this.limit = limit;
     }
 
     List<Integer> getPrimes() {
         BitSet bits = new BitSet();
         List<Integer> list = new ArrayList<Integer>();
 
-        for (int i = 2; i < limit; i++) {
+        for (int i = 2; i <= limit; i++) {
             if (!bits.get(i)) {
                 list.add(i);
             }
-            for (int j = i+i; j < limit; j+=i) {
+            for (int j = i+i; j <= limit; j+=i) {
                 bits.set(j);
             }
         }
