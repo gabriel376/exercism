a6a53472458ce31954ffa8ad0bd107d2b799017e
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-10-05 20:53:35 -0300

    python largest-series-product iteration #2 small refactoring

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -5,6 +5,6 @@ def largest_product(series, size):
     if size < 0:
         raise ValueError('Invalid size')
 
-    chunks = (series[i:i + size] for i in range(len(series) - size + 1))
-    products = (reduce(mul, map(int, chunk), 1) for chunk in map(list, chunks))
+    chunks = (series[i:i+size] for i in range(len(series)-size+1))
+    products = (reduce(mul, map(int, chunk), 1) for chunk in chunks)
     return max(products)
