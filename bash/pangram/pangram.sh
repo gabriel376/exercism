6cd62ea5b9c4b0013d4fdbcd3938fedc56b2ba70
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-11-24 19:27:22 -0300

    bash pangram iteration #1

---
+++ bash/pangram/pangram.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function main {
+    string=${1^^*}
+    for char in {A..Z}; do
+        [[ $string =~ $char ]] || end "false"
+    done
+    end "true"
+}
+
+function end {
+    echo ${1}
+    exit 0
+}
+
+main "$@"
