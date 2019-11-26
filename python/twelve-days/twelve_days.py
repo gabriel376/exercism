1a73a8c0e48ca98ad58d1aa6ce66681918f33ee6
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-11-26 19:10:21 -0300

    python twelve-days iteration #2 small refactoring

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,24 +1,37 @@
-LYRICS = [
-    ['first',    'a Partridge in a Pear Tree.'],
-    ['second',   'two Turtle Doves'],
-    ['third',    'three French Hens'],
-    ['fourth',   'four Calling Birds'],
-    ['fifth',    'five Gold Rings'],
-    ['sixth',    'six Geese-a-Laying'],
-    ['seventh',  'seven Swans-a-Swimming'],
-    ['eighth',   'eight Maids-a-Milking'],
-    ['ninth',    'nine Ladies Dancing'],
-    ['tenth',    'ten Lords-a-Leaping'],
-    ['eleventh', 'eleven Pipers Piping'],
-    ['twelfth',  'twelve Drummers Drumming'],
+ORDINALS = [
+    'first',
+    'second',
+    'third',
+    'fourth',
+    'fifth',
+    'sixth',
+    'seventh',
+    'eighth',
+    'ninth',
+    'tenth',
+    'eleventh',
+    'twelfth',
 ]
 
-ORDINALS = [line[0] for line in LYRICS]
-GIFTS =    [line[1] for line in LYRICS]
+GIFTS = [
+    'a Partridge in a Pear Tree.',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming',
+]
+
+VERSE = 'On the {} day of Christmas my true love gave to me: {}{}{}'
 
 def get_verse(n):
-    verse = 'On the {} day of Christmas my true love gave to me: {}{}{}'
-    return verse.format(
+    return VERSE.format(
         ORDINALS[n-1],
         ', '.join(GIFTS[1:n][::-1]),
         ', and ' if n > 1 else '',
