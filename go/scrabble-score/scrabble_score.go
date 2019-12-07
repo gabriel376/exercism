f4181c14ec759d9a0f5f7f6d572fc46e588bf22e
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-12-07 19:19:43 -0300

    go scrabble-score iteration #4 use switch case

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,38 +1,28 @@
 package scrabble
 
 import (
-	"strings"
+	"unicode"
 )
 
-var scoresByChar = make(map[byte]int)
-var scoresByValue = map[int]string{
-	1:  "AEIOULNRST",
-	2:  "DG",
-	3:  "BCMP",
-	4:  "FHVWY",
-	5:  "K",
-	8:  "JX",
-	10: "QZ",
-}
-
-func init() {
-	for score, upper := range scoresByValue {
-		lower := strings.ToLower(upper)
-		for i := range upper {
-			scoresByChar[upper[i]] = score
-			scoresByChar[lower[i]] = score
-		}
-	}
-}
-
-// Score calculates the scrabble score
-// for a given word. Characters not in
-// range a-zA-Z are ignored.
-func Score(text string) int {
-	total := 0
-	for i := range text {
-		if score, ok := scoresByChar[text[i]]; ok {
-			total += score
+// Score calculates the scrabble score for a given word.
+// Characters not in range a-zA-Z are ignored.
+func Score(text string) (total int) {
+	for _, r := range text {
+		switch unicode.ToLower(r) {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			total++
+		case 'd', 'g':
+			total += 2
+		case 'b', 'c', 'm', 'p':
+			total += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			total += 4
+		case 'k':
+			total += 5
+		case 'j', 'x':
+			total += 8
+		case 'q', 'z':
+			total += 10
 		}
 	}
 	return total
