012577267d9dafae1298e930c6192a7f12f4b6ed
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2020-05-24 03:47:50 -0300

    clojure grade-school iteration #1

---
+++ clojure/grade-school/src/grade_school.clj
@@ -0,0 +1,10 @@
+(ns grade-school)
+
+(defn grade [school grade]
+  (get school grade []))
+
+(defn add [school name grade]
+  (merge-with concat school {grade [name]}))
+
+(defn sorted [school]
+  (into (sorted-map) (for [[grade names] school] {grade (sort names)})))
