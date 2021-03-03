1a799e7c01a44a1095560526b415e618288ea286
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2021-03-03 11:42:31 -0300

    c sum-of-multiples iteration #2
    version update

--- c/sum-of-multiples/src/sum_of_multiples.h
+++ c/sum-of-multiples/src/sum_of_multiples.h
@@ -1,8 +1,8 @@
 #ifndef SUM_OF_MULTIPLES_H
 #define USM_OF_MULTIPLES_H
 
-unsigned int sum_of_multiples(const unsigned int *multiples,
-                              unsigned int len,
-                              unsigned int num);
+unsigned int sum(const unsigned int *multiples,
+                 unsigned int len,
+                 unsigned int num);
 
 #endif
