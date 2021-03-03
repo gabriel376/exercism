317cbcc45f34bae90f072c120fe2004e81d88bb4
Gabriel do Nascimento Ribeiro <gabriel.nascimento@nubank.com.br>
2021-03-03 11:42:31 -0300

    c sum-of-multiples iteration #2
    version update

--- c/sum-of-multiples/src/sum_of_multiples.c
+++ c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
 #include "sum_of_multiples.h"
 
-unsigned int sum_of_multiples(const unsigned int * const multiples,
-                              const unsigned int len,
-                              const unsigned int num) {
-  unsigned int sum = 0;
+unsigned int sum(const unsigned int * const multiples,
+                 const unsigned int len,
+                 const unsigned int num) {
+
+  unsigned int result = 0;
 
   if (multiples == NULL) {
-    return sum;
+    return result;
   }
 
   for (unsigned int i = 1; i < num; i++) {
     for (unsigned int j = 0; j < len; j++) {
       if (multiples[j] > 0 && i % multiples[j] == 0) {
-        sum += i;
+        result += i;
         break;
       }
     }
   }
 
-  return sum;
+  return result;
 }
