db6f36a3ae6dc74386d9ed4becd899bd4b6eff99
Gabriel <gabriel376@hotmail.com>
2022-03-28 03:32:07 -0300

    go robot-name iteration #2

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -6,66 +6,70 @@ import (
 	"time"
 )
 
+var (
+	names []string
+)
+
+func product(xs ...[]string) []string {
+	if len(xs) == 1 {
+		return xs[0]
+	}
+
+	prod := make([]string, 0, len(xs))
+	for _, v1 := range xs[0] {
+		for _, v2 := range xs[1] {
+			prod = append(prod, v1+v2)
+		}
+	}
+
+	args := make([][]string, 0, len(xs)-1)
+	args = append(args, prod)
+	args = append(args, xs[2:]...)
+
+	return product(args...)
+}
+
+func init() {
+	letters := make([]string, 0, 26)
+	for c := 'A'; c <= 'Z'; c++ {
+		letters = append(letters, string(c))
+	}
+
+	digits := make([]string, 0, 10)
+	for c := '0'; c <= '9'; c++ {
+		digits = append(digits, string(c))
+	}
+
+	names = product(letters, letters, digits, digits, digits)
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(names), func(i, j int) {
+		names[i], names[j] = names[j], names[i]
+	})
+}
+
 // Robot represents a robot.
 type Robot struct {
 	name string
 }
 
-type nameConfigPart struct {
-	start rune
-	size  int
-}
-
-var max int
-var index int
-var numbers []int
-var config = []nameConfigPart{
-	{'A', 26},
-	{'A', 26},
-	{'0', 10},
-	{'0', 10},
-	{'0', 10},
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-
-	max = 1
-	for _, elem := range config {
-		max *= elem.size
-	}
-
-	index = 0
-	numbers = rand.Perm(max)
-}
-
-// Reset clears robot name.
+// Reset clears the robot name.
 func (robot *Robot) Reset() {
 	robot.name = ""
 }
 
-// Name returns robot name.
+// Name returns the robot name.
 func (robot *Robot) Name() (string, error) {
-	if index >= max {
-		return "", errors.New("names exhausted")
-	}
-
 	if robot.name != "" {
 		return robot.name, nil
 	}
 
-	num := numbers[index]
-	runes := []rune{}
-	prod := 1
-
-	for _, elem := range config {
-		r := rune(num/prod%elem.size) + elem.start
-		runes = append(runes, r)
-		prod *= elem.size
+	if len(names) == 0 {
+		return "", errors.New("No more names available")
 	}
 
-	index++
-	robot.name = string(runes)
+	robot.name = names[0]
+	names = names[1:]
 
 	return robot.name, nil
 }
