4b7dcd7d74993eb059a9d0f961d4087929e95bad
Gabriel <gabriel376@hotmail.com>
2022-03-27 22:20:10 -0300

    python robot-name iteration #2

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,19 +1,30 @@
-from string import ascii_uppercase as letters
-from string import digits
-from random import shuffle
 from itertools import product
+from random import shuffle
+from string import ascii_uppercase, digits
 
-base = [letters, letters, digits, digits, digits]
-names = list(product(*base))
-shuffle(names)
 
-class Robot(object):
+class RobotNames():
+    _all_names = [''.join(chars) for chars in product(*[
+        ascii_uppercase,
+        ascii_uppercase,
+        digits,
+        digits,
+        digits,
+    ])]
+
     def __init__(self):
+        names = self._all_names.copy()
+        shuffle(names)
+        self._names = iter(names)
+
+    def __next__(self):
+        return next(self._names)
+
+
+class Robot():
+    def __init__(self):
+        self._names = RobotNames()
         self.reset()
 
     def reset(self):
-        if not names:
-            raise ValueError('All possible names have been created.')
-        self.name = ''.join(names.pop())
-
-
+        self.name = next(self._names)
