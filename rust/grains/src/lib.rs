f95d36b859b5a83647837cf50918c95f4c089916
Gabriel <gabriel376@hotmail.com>
2022-03-28 15:40:14 -0300

    rust grains iteration #3

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -3,7 +3,7 @@ const MAX: u32 = 64;
 
 pub fn square(num: u32) -> u64 {
     match num {
-        MIN...MAX => 2u64.pow(num - 1),
+        MIN..=MAX => 2u64.pow(num - 1),
         _ => panic!("Square must be between {} and {}", MIN, MAX),
     }
 }
