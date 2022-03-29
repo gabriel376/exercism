a6110a93254f715fc8b3b3575949a8f97676c29a
Gabriel <gabriel376@hotmail.com>
2022-03-29 01:21:09 -0300

    python pythagorean-triplet iteration #2

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,8 @@
 def triplets_with_sum(total):
-    return set(pythagorean_triplets(total))
-
-def pythagorean_triplets(total):
+    triplets = []
     for a in range(1, total):
-        b = (pow(total - a, 2) - pow(a, 2)) / (2 * (total - a))
+        b = int((pow(total - a, 2) - pow(a, 2)) / (2 * (total - a)))
         c = total - a - b
-
-        if a < b and b == int(b) and a*a + b*b == c*c:
-            yield (a, b, c)
+        if a < b and pow(a, 2) + pow(b, 2) == pow(c, 2):
+            triplets.append([a, b, c])
+    return triplets
