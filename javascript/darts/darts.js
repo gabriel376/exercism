1e9bf9d98817ef5dcc990a6b8c5a8615e694f1cb
Gabriel <gabriel376@hotmail.com>
2022-03-29 02:15:46 -0300

    javascript darts iteration #2

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,29 +1,11 @@
 const SCORES = [
-    {
-        from: 0,
-        to: 1,
-        value: 10,
-    },
-    {
-        from: 1,
-        to: 5,
-        value: 5,
-    },
-    {
-        from: 5,
-        to: 10,
-        value: 1,
-    },
-    {
-        from: 10,
-        to: Number.MAX_SAFE_INTEGER,
-        value: 0,
-    }
+    { from: 0,  to: 1,  value: 10 },
+    { from: 1,  to: 5,  value: 5  },
+    { from: 5,  to: 10, value: 1  },
 ];
 
-export const solve = (x, y) => {
+export function score(x, y) {
     let dist = Math.sqrt(x*x + y*y);
-    return isNaN(dist)
-        ? null
-        : SCORES.find(s => dist >= s.from && dist <= s.to).value;
+    let score = SCORES.find(s => dist >= s.from && dist <= s.to);
+    return score && score.value || 0
 }
