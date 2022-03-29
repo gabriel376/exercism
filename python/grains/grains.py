4aef7edf27e7a605edcb2273bf1c4949db6747e8
Gabriel <gabriel376@hotmail.com>
2022-03-29 00:20:07 -0300

    python grains iteration #2

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,14 +1,7 @@
-def validate(fn):
-    def wrapper(num):
-        if num < 1 or num > 64:
-            raise ValueError("invalid input")
-        return fn(num)
-    return wrapper
-
-@validate
-def on_square(num):
+def square(num):
+    if num < 1 or num > 64:
+        raise ValueError("square must be between 1 and 64")
     return 1 << (num - 1)
 
-@validate
-def total_after(num):
-    return (1 << num) - 1
+def total():
+    return (1 << 64) - 1
