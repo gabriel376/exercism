c648baeef85230f981f5e05a2fbc003a4a28f3c2
Gabriel <gabriel376@hotmail.com>
2022-03-29 01:26:08 -0300

    python saddle-points iteration #3

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,12 +1,10 @@
 def saddle_points(matrix):
     if len(set(map(len, matrix))) > 1:
-        raise ValueError('irregular matrix not allowed')
+        raise ValueError('irregular matrix')
 
     t_matrix = list(zip(*matrix))
 
-    points = [(x+1, y+1)
-              for x, row in enumerate(matrix)
-              for y, col in enumerate(t_matrix)
-              if matrix[x][y] == max(row) and matrix[x][y] == min(col)]
-
-    return set(points)
+    return [{"row": x+1, "column": y+1}
+            for x, row in enumerate(matrix)
+            for y, col in enumerate(t_matrix)
+            if matrix[x][y] == max(row) and matrix[x][y] == min(col)]
