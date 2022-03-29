4a2c82d03908abcbe763b8cba4fdeee3afd313bd
Gabriel <gabriel376@hotmail.com>
2022-03-28 23:32:51 -0300

    python phone-number iteration #2

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,34 +1,49 @@
-class Phone:
+import re
+
+
+class PhoneNumber:
     def __init__(self, phone):
-        # extract only digits
+        if re.match(r'.*[a-zA-Z]', phone):
+            raise ValueError('letters not permitted')
+
+        if re.match(r'.*[@:!]', phone):
+            raise ValueError('punctuations not permitted')
+
         number = ''.join(filter(str.isdigit, phone))
 
         if len(number) == 11:
-            # check country code
             if number[0] != '1':
-                raise ValueError('Invalid Country Code')
-            # remove country code
+                raise ValueError('11 digits must start with 1')
             number = number[1:]
 
-        # check length
-        if len(number) != 10:
-            raise ValueError('Invalid Length')
+        if len(number) < 10:
+            raise ValueError('incorrect number of digits')
 
-        # check area code
-        if number[0] in '01':
-            raise ValueError('Invalid Area Code')
+        if len(number) > 10:
+            raise ValueError('more than 11 digits')
 
-        # check exchange code
-        if number[3] in '01':
-            raise ValueError('Invalid Exchange Code')
+        area_code = number[:3]
 
-        self.number        = number
-        self.area_code     = number[:3]
-        self.exchange_code = number[3:6]
-        self.line_number   = number[6:]
+        if area_code[0] == '0':
+            raise ValueError('area code cannot start with zero')
+
+        if area_code[0] == '1':
+            raise ValueError('area code cannot start with one')
+
+        exchange_code = number[3:6]
+
+        if exchange_code[0] == '0':
+            raise ValueError('exchange code cannot start with zero')
+
+        if exchange_code[0] == '1':
+            raise ValueError('exchange code cannot start with one')
+
+        line_number = number[6:]
+
+        self.number = number
+        self.area_code = area_code
+        self.exchange_code = exchange_code
+        self.line_number = line_number
 
     def pretty(self):
-        return '({}) {}-{}'.format(
-                self.area_code,
-                self.exchange_code,
-                self.line_number)
+        return f'({self.area_code})-{self.exchange_code}-{self.line_number}'
